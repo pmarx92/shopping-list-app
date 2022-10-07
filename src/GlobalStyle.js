ef5ad6c7a58a40719347096c272ed32253d99324
Patrick <pamarx92@gmail.com>
2022-10-07 11:22:25 +0200

    project setup

---
+++ src/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+ 
+const GlobalStyle = createGlobalStyle`
+* {
+ box-sizing: border-box;
+ font-family: 'Inter', sans-serif;
+}
+ 
+body {
+ font-family: sans-serif;
+ margin: 0;
+ 
+}
+`;
+ 
+export default GlobalStyle;
